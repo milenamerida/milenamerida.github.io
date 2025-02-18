34ac688f464696d5d8b0299d691d9126a2191379
Milena Leticia Carreira Merida <co75628@mail.harpercollege.edu>
2025-02-18 11:10:49 -0600

    Update M6P1.py

--- CIS106/M6/M6P1.py
+++ CIS106/M6/M6P1.py
@@ -1,17 +1,11 @@
 widget = int(input('Enter the quantity of widgets: '))
 if widget > 10000:
   price = 10.00
-  extprice = widget*price
-  tax = extprice*0.07
-  total = tax+extprice
 elif widget >= 5000:
   price = 20.00
-  extprice = widget*price
-  tax = extprice*0.07
-  total = tax+extprice
 else:
   price = 30.00
-  extprice = widget*price
-  tax = extprice*0.07
-  total = tax+extprice
+extprice = widget*price
+tax = extprice*0.07
+total = tax+extprice
 print('Extended price ${:.2f}, tax amount ${:.2f} and total ${:.2f}'.format(extprice, tax, total))
