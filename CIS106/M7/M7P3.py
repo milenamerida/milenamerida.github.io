8e74d9148733bd3faaace18a495ec382057007a7
Milena Leticia Carreira Merida <co75628@mail.harpercollege.edu>
2025-02-27 10:28:51 -0600

    Create M7P3.py

---
+++ CIS106/M7/M7P3.py
@@ -0,0 +1,9 @@
+prompt = input('Do you want to run this program? (yes/no) ')
+if prompt == 'yes':
+  while prompt == 'yes':
+    lastname = input('Enter last name: ')
+    score1 = int(input('Enter first score: '))
+    score2 = int(input('Enter second score: '))
+    average = (score1 + score2) / 2
+    print('Average for {} is {:.0f}'.format(lastname, average))
+    prompt = input('Do you want to run this program again? (yes/no) ')
